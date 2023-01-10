991e75054df3e8ed3ad4b60dd18ad3f1d3ea4811
Jack Widman <jack.widman@gmail.com>
2023-01-10 09:19:57 -0500

    hooked up mood tone

--- Profile.js
+++ Profile.js
@@ -17,42 +17,13 @@ import { useRef } from 'react';
 import Svg, {Circle,Defs,RadialGradient, Stop} from 'react-native-svg';
 
 
-const pieData2 = [
-    {
-      name: 'Bitcoin',
-      population: 63,
-      color: '#00BFFF'
-    },
-    {
-      name: 'Dogecoin',
-      population: 9,
-      color: 'gold'
-    },
-    {
-      name: 'Ethereum',
-      population: 19,
-      color: 'orange'
-    },
-    {
-      name: 'Tether',
-      population: 6,
-      color: 'green'
-    },
-    {
-      name: 'Polygon',
-      population: 3,
-      color: 'red'
-    },
-];
 
 const pieData = (moods) => {
-  console.log('inside pieData');
-
+  console.log("Inside pieData. count=" + moods.length);
+  
   const result = [];
 
   const count = new Set(moods.map(m=>m.word)).size;
-
-  console.log("count=" + count);
 
   const factor = Math.round(100/count);
 
@@ -68,7 +39,7 @@ const pieData = (moods) => {
 
     const rgb = getDefaultColorFromMood(mood);
     console.log("rgb=" + rgb[0] + '. ' + rgb[1] + ', ' + rgb[2])
-
+    console.log('setting color field to ' + convertToHex([rgb[0],rgb[1],rgb[2]]));
     pd['color'] = convertToHex([rgb[0],rgb[1],rgb[2]]);
 
     result.push(pd)
@@ -94,8 +65,6 @@ async function checkUserLogin(n) {
   }).catch(error => console.log(error));
 }
 
-
-
 const Profile = ({route}) => {
 
     const { item } = route.params;
@@ -105,20 +74,21 @@ const Profile = ({route}) => {
     const [data, setData] = useState([]);
     const [pie, setPie] = useState([]);
 
+    const [color1, setColor1] = useState(["#FFFFFF"]);
+    const [color2, setColor2] = useState(["#FFFFFF"]);
+    const [color3, setColor3] = useState(["#FFFFFF"]);
+    const [color4, setColor4] = useState(["#FFFFFF"]);
+    const [color5, setColor5] = useState(["#FFFFFF"]);
+    const [color6, setColor6] = useState(["#FFFFFF"]);
+    const [color7, setColor7] = useState(["#FFFFFF"]);
+    const [color8, setColor8] = useState(["#FFFFFF"]);
+
+
   const navigation = useNavigation();  
 
   checkUserLogin(navigation)
 
   const imgSrc = avatar() + item.email + '.jpg';
-
-  ////////////////////////////////////////////////////////////////
-
- const showMoodRing = (canvas) => {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(0, 0, 100, 100);
-  }
- /////////////////////////////////////////////////////
 
   async function fetchData() {
     try {
@@ -131,10 +101,63 @@ const Profile = ({route}) => {
       console.log('The number of moods is ' + data.length);
       const color = getAverageColor(data);
       setAverageColor(color);
-      
       setPie(pieData(data));
 
+      console.log('About to call toMood ring on ' + data)
       
+      const colors = data.map(function(m) {return convertToHex([m.red,m.green,m.blue])})
+      
+      console.log('color1=' + colors[0]);
+      console.log('color2=' + colors[1]);
+      console.log('color3=' + colors[2]);
+      console.log('color4=' + colors[3]);
+      console.log('color5=' + colors[4]);
+      console.log('color6=' + colors[5]);
+      console.log('color7=' + colors[6]);
+      console.log('color8=' + colors[7]);
+      if (colors[0] != undefined) {
+        setColor1(colors[0]);
+      } else {
+        setColor1("#FFFFFF")
+      }
+      if (colors[1] != undefined) {
+        setColor2(colors[1]);
+      } else {
+        setColor2("#FFFFFF")
+      }
+      if (colors[2] != undefined) {
+        setColor3(colors[2]);
+      } else {
+        setColor3("#FFFFFF")
+      }
+      if (colors[3] != undefined) {
+        setColor4(colors[3]);
+      } else {
+        setColor4("#FFFFFF")
+      }
+      if (colors[4] != undefined) {
+        setColor5(colors[4]);
+      } else {
+        setColor5("#FFFFFF")
+      }
+      if (colors[5] != undefined) {
+        setColor6(colors[5]);
+      } else {
+        setColor6("#FFFFFF")
+      }
+      if (colors[6] != undefined) {
+        setColor7(colors[6]);
+      } else {
+        setColor7("#FFFFFF")
+      }
+      if (colors[7] != undefined) {
+        setColor8(colors[7]);
+      } else {
+        setColor8("#FFFFFF")
+      }
+
+
+
       
     } catch (error) {
       console.error(error);
@@ -167,14 +190,14 @@ const Profile = ({route}) => {
       fy="15"
       gradientUnits="userSpaceOnUse"
     >
-      <Stop offset="10%" stopColor="white" stopOpacity="1" />
-      <Stop offset="20%" stopColor="red" stopOpacity="1" />
-      <Stop offset="30%" stopColor="blue" stopOpacity="1" />
-      <Stop offset="40%" stopColor="green" stopOpacity="1" />
-      <Stop offset="50%" stopColor="pink" stopOpacity="1" />
-      <Stop offset="60%" stopColor="purple" stopOpacity="1" />
-      <Stop offset="70%" stopColor="yellow" stopOpacity="1" />
-      <Stop offset="80%" stopColor="green" stopOpacity="1" />
+      <Stop offset="0" stopColor={color1} stopOpacity="1" />
+      <Stop offset="0.1" stopColor={color2} stopOpacity="1" />
+      <Stop offset="0.3" stopColor={color3} stopOpacity="1" />
+      <Stop offset="0.5" stopColor={color4} stopOpacity="1" />
+      <Stop offset="0.7" stopColor={color5} stopOpacity=".5" />
+      <Stop offset="0.8" stopColor={color6} stopOpacity="1" />
+      <Stop offset="0.9" stopColor={color7} stopOpacity="1" />
+      <Stop offset="1" stopColor={color8} stopOpacity="1" />
     </RadialGradient>
   </Defs>
   <Circle  width={200} height={200} cx="150" cy="75" r="75" fill="url(#grad)" />
